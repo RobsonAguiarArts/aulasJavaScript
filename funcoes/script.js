5378314fa14e5250d233b68f7c6a24f54a87cb2b
Robson Aguiar <robsonaguiarart@hotmail.com>
2022-11-13 21:48:32 -0300

    Correção dos exercicios

--- funcoes/script.js
+++ funcoes/script.js
@@ -8,29 +8,41 @@ function reprovou(notaProva){
   }
 }
 console.log(reprovou(5));
+//  resposta do Professor 
+function isTruthy(dado){
+  return !!dado;
+}
+console.log(isTruthy(10));
 // Crie uma função matemática que retorne o perímetro de um quadrado
 // lembrando: perímetro é a soma dos quatro lados do quadrado
 function perimetroQuadrado(ladoQuadrado){
-  var perimetro = ladoQuadrado + ladoQuadrado;
+  var perimetro = ladoQuadrado * 4;
   return perimetro;
 }
 console.log(perimetroQuadrado(10));
+//Resolução do professor 
+function perimetroQuadrado2(lado){
+  return lado *4 
+}
 // Crie uma função que retorne o seu nome completo
 // ela deve possuir os parâmetros: nome e sobrenome
 function nomeCompleto(nome,sobrenome){
 return` ${nome} ${sobrenome}`;
 }
 console.log(nomeCompleto('Robson','Aguiar'));
+//resolução do propfessor foi a mesma.
 
 // Crie uma função que verifica se um número é par
 function numeroPar(numero){
-  if(numero % 2 == 0 ){
+  if(numero % 2 === 0 ){
     return `O número digitado foi ${numero} e o resto da divisão dele é ${numero % 2} então ele é par`;
   }else{
     return `O número digitado foi ${numero} e o resto da divisão dele é ${numero % 2} então ele é impar`;
   }
 }
 console.log(numeroPar(3));
+
+
 // Crie uma função que retorne o tipo de
 // dado do argumento passado nela (typeof)
 function restanteDeFerias(diasDeferiasJaTiradas){
@@ -61,3 +73,5 @@ function jaVisitei(paisesVisitados) {
 }
 console.log(precisoVisitar(20));
 console.log(jaVisitei(20));
+
+// totalPaises não estava definido na segunda função apenas na primeira, foi preciso mover para o escopo das funções.
