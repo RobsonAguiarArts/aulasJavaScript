6f69edb06aed83dfc5816e557d811f9d25cd0bc8
Robson Aguiar <robson.aguiarart@hotmail.com>
2022-11-16 17:35:20 -0300

    Aula e exercicios Atribuição e Ternário

---
+++ atribuicao-e-ternario/script.js
@@ -0,0 +1,16 @@
+// Some 500 ao valor de scroll abaixo,
+// atribuindo o novo valor a scroll
+var scroll = 1000;
+console.log(scroll += 500);
+
+// Atribua true para a variável darCredito,
+var darCredito;
+var possuiCarro = true;
+var possuiCasa = true;
+// caso o cliente possua carro e casa.
+darCredito = (possuiCarro && possuiCasa);
+  console.log(darCredito);
+
+// E false caso o contrário.
+
+
